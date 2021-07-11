afa95827b76d92bc67665c6f62f86a17ad3d7988
jguerrero142 <julgueover.05@gmail.com>
2021-07-11 17:44:08 -0500

    Store sin NG zorro

--- src/app/components/total-pedido/total-pedido.component.ts
+++ src/app/components/total-pedido/total-pedido.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { PedidoService } from 'src/app/services/pedido.service';
+import { User } from '../../models/User';
+import { UsersService } from '../../services/user.service';
 
 @Component({
   selector: 'app-total-pedido',
@@ -8,13 +12,48 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TotalPedidoComponent implements OnInit {
 
 
-  @Input() user: number;
-  constructor() { 
+  id: any = [];
+  user$: any = [];
+  user: User;
+  pedidos: any = [];
+  idPedidos: any = [];
+  idPedido: number;
+  dataUser: any = [];
+
+  // @Input() user: number;
+  constructor(public auth: AuthService,
+              public pedidoServices: PedidoService,
+              public userServices: UsersService,) { 
     
   }
   
   ngOnInit(): void {
     
+  }
+  validarUser(){
+    this.auth.userProfile$.subscribe((perfil: User) => {
+    this.user$ = perfil;
+        if(this.user$){
+        // console.log(this.user$.sub);
+        this.getUser();
+        
+     }
+    })
+  }
+  getUser(){ 
+    this.userServices.getUser(this.user$.sub)
+    .subscribe(res => {
+        this.user = res;
+        this.id = this.user.id_user;
+        console.log(this.id);
+        this.getPedidos();
+    })
+  }
+  getPedidos(){
+    this.pedidoServices.getUserPedidos(this.id)
+    .subscribe(resp => {
+      this.pedidos = resp;
+    })
   }
 
 }
